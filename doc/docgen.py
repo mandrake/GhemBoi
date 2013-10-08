5072d06c11a745ab93a91398bad948853e8521ed
mandrake <wandrake@gmail.com>
2013-10-08 22:26:06 +0200

    New style for documentation

--- doc/docgen.py
+++ doc/docgen.py
@@ -13,15 +13,15 @@ j = with_file(json.load, '../src/opcodes/z80gb.json', 'r', '%')
 
 t = env.get_template('opcodes-table.html')
 f = open('opcodes-table.html', 'w')
-f.write(t.render(prefix = '', opcodes = j))
+f.write(t.render(prefix = '', opcodes = j).encode('utf-8'))
 f.close()
 
 t = env.get_template('opcodes-list.html')
 f = open('opcodes-list.html', 'w')
-f.write(t.render(prefix = '', opcodes = j))
+f.write(t.render(prefix = '', opcodes = j).encode('utf-8'))
 f.close()
 
 t = env.get_template('instructions/nop.html')
 f = open('nop.html', 'w')
-f.write(t.render())
+f.write(t.render().encode('utf-8'))
 f.close()
