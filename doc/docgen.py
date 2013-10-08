5b7d119ce34cd860abaa2a81d37a4791d9ff8825
mandrake <wandrake@gmail.com>
2013-10-08 10:58:21 +0200

    Jinja templating

---
+++ doc/docgen.py
@@ -0,0 +1,22 @@
+from jinja2 import Environment, FileSystemLoader
+import json
+
+env = Environment(loader = FileSystemLoader('./templates'))
+
+def with_file(fun, fname, mode, *params):
+    f = open(fname, mode)
+    x = fun(*map(lambda x: f if x == '%' else x, params))
+    f.close()
+    return x
+
+j = with_file(json.load, '../src/opcodes/z80gb.json', 'r', '%')
+
+t = env.get_template('opcodes-table.html')
+f = open('opcodes-table.html', 'w')
+f.write(t.render(prefix = '', opcodes = j))
+f.close()
+
+t = env.get_template('opcodes-list.html')
+f = open('opcodes-list.html', 'w')
+f.write(t.render(prefix = '', opcodes = j))
+f.close()
